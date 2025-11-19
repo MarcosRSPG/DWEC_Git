e8c98fb665e9c901c45175f5d50af6726ae14dda
MarcosRSPG <marcosrs.softwaredeveloper@gmail.com>
2025-11-19 19:36:24 +0100

    Ejercicio palabras comenzado

---
+++ Unidad_5/EjercicioJuegoPalabras/js/login.js
@@ -0,0 +1,61 @@
+import { USER_PASS } from "./constantes.js";
+
+let arrUP = recogerUsuarios();
+
+const submitLogin = document.getElementById("submitLogin");
+const inputNombre = document.getElementById("inputUsuario");
+const inputPassword = document.getElementById("inputPassword");
+const passErr = document.getElementById("passError");
+const userErr = document.getElementById("userError");
+
+submitLogin.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  userErr.textContent = passErr.textContent = "";
+  let user = {
+    nombre: inputNombre.value,
+    password: inputPassword.value,
+  };
+  let existenceCheck = comprobarExistencia(user);
+  let formatCheck = comprobarFormat(user);
+  if (existenceCheck) {
+    window.location.href = "juego.html";
+  } else {
+    if (formatCheck) {
+      newUsuario(user);
+      window.location.href = "juego.html";
+    }
+  }
+});
+function comprobarFormat(user) {
+  let reu = /^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/;
+  let rep = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*]).{8,}$/;
+  let check = false;
+  !reu.test(user.nombre)
+    ? (userErr.textContent = "El nombre debe contener solo letras y numeros")
+    : !rep.test(user.password)
+    ? (passErr.textContent =
+        "La contraseña debe contener mayusculas, minusculas, numeros y un simbolo de los siguientes: !@#$%^&*")
+    : (check = true);
+  return check;
+}
+function comprobarExistencia(user) {
+  arrUP.forEach((usuario) => {
+    if (usuario.nombre === user.nombre) {
+      if (usuario.password === user.password) {
+        return true;
+      }
+      passErr.textContent = "La contraseña no es correcta para este usuario";
+      return false;
+    }
+  });
+  return false;
+}
+function recogerUsuarios() {
+  return JSON.parse(localStorage.getItem(USER_PASS) || "[]");
+}
+function newUsuario(user) {
+  arrUP.push(user);
+  localStorage.setItem(USER_PASS, JSON.stringify(arrUP));
+  window.location.href = "juego.html";
+}
