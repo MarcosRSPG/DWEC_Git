324fed76fe36e7ddb7d936ce531a2b6c34bd0f45
MarcosRSPG <marcosrs.softwaredeveloper@gmail.com>
2025-11-30 17:07:37 +0100

    examen globos completado

--- UT5_WEB_Practico/js/globos.js
+++ UT5_WEB_Practico/js/globos.js
@@ -4,47 +4,40 @@ const seccion = document.getElementById("globos");
 const imgGlobos = [
   { tipo: "rojo", src: "./img/redCircle.jpg" },
   { tipo: "verde", src: "./img/Green_Circle.png" },
+  { tipo: "azul", src: "./img/blue_Circle.png" },
 ];
-/* let tiempo = 0;
-let puntos = 0;
-let numVerdes = 0;
-let fallos = 0;
-generarGlobos(Number.parseInt(localStorage.getItem(NUM_GLOB)));
-
-setInterval(() => moverGlobos(), 100); */
-
-generarGlobos(Number.parseInt(localStorage.getItem(NUM_GLOB)));
-
-setInterval(() => moverGlobos(), 100);
 let tiempo = 0;
 let puntos = 0;
 let numVerdes = 0;
 let fallos = 0;
+let multiplicador = 1;
+let ejecutado = false;
+generarGlobos(Number.parseInt(localStorage.getItem(NUM_GLOB)));
+
+let id1 = setInterval(() => moverGlobos(), 100);
 
 function generarGlobos(num) {
   for (let index = 0; index < num - 1; index++) {
-    let imgGlobo = Math.floor(Math.random() * 2);
+    let imgGlobo = Math.floor(Math.random() * 3);
     let template = document.getElementById("tempGlob");
     let node = template.content.firstElementChild.cloneNode(true);
     node.querySelector("#imagen").setAttribute("src", imgGlobos[imgGlobo].src);
-    /*     node.setAttribute("tipo", imgGlobos[imgGlobo].tipo);
+    node.setAttribute("tipo", imgGlobos[imgGlobo].tipo);
     if (node.getAttribute("tipo") === "verde") {
       numVerdes++;
     }
     node.addEventListener("click", (event) => {
-      if (node.getAttribute("tipo") === "verde") { 
+      if (node.getAttribute("tipo") === "verde") {
         puntos++;
-      } */
-    node
-      .querySelector("#imagen")
-      .setAttribute("tipo", imgGlobos[imgGlobo].tipo);
-    node.addEventListener("click", (event) => {
-      if (imgGlobos[imgGlobo].tipo === "verde") {
-        puntos++;
-        numVerdes++;
       }
-      if (imgGlobos[imgGlobo].tipo === "rojo") {
+      if (node.getAttribute("tipo") === "rojo") {
         fallos++;
+      }
+      if (node.getAttribute("tipo") === "azul") {
+        clearInterval(id1);
+        setTimeout(() => {
+          id1 = setInterval(() => moverGlobos(), 100);
+        }, 2000);
       }
       seccion.removeChild(node);
     });
@@ -57,6 +50,9 @@ function generarGlobos(num) {
   node.querySelector("#imagen").setAttribute("src", "./img/Yellow_Circle.png");
   node.querySelector("#imagen").setAttribute("tipo", "amarillo");
   node.addEventListener("click", (event) => {
+    if (tiempo <= 3000) {
+      multiplicador = 2;
+    }
     seccion.removeChild(node);
   });
   node.style.top = CSS.percent(Math.random() * 80);
@@ -73,12 +69,13 @@ function moverGlobos() {
 
     tiempo += 100;
   });
-  if (comprobarVerdes()) {
-    let puntuacion = 1000 / (tiempo / 1000) + puntos * 10 - fallos * 5;
+  if (comprobarVerdes() && !ejecutado) {
+    let puntuacion =
+      (1000 / (tiempo / 1000) + puntos * 10 - fallos * 5) * multiplicador;
     localStorage.setItem(PUNTUACION, puntuacion);
-    /* alert(`Tu puntuacion es: ${puntuacion}`);
-    window.location.href = "numGlob.html"; */
-    window.location.href = "puntuaciones.html";
+    alert(`Tu puntuacion es: ${puntuacion}`);
+    ejecutado = true;
+    window.location.href = "numGlob.html";
   }
 }
 function comprobarVerdes() {
@@ -88,19 +85,15 @@ function moverHorizontal(node, direccion) {
   if (direccion === "left" && parseFloat(node.style.left) <= 97) {
     node.style.left = (parseFloat(node.style.left) || 0) + 0.1 + "%";
   }
-  /* if (direccion === "right" && parseFloat(node.style.left) >= 0) {
-    node.style.left = (parseFloat(node.style.left) || 0) - 0.1 + "%"; */
-  if (direccion === "right" && parseFloat(node.style.right) <= 97) {
-    node.style.right = (parseFloat(node.style.right) || 0) + 0.1 + "%";
+  if (direccion === "right" && parseFloat(node.style.left) >= 0) {
+    node.style.left = (parseFloat(node.style.left) || 0) - 0.1 + "%";
   }
 }
 function moverVertical(node, direccion) {
   if (direccion === "top" && parseFloat(node.style.top) <= 97) {
     node.style.top = (parseFloat(node.style.top) || 0) + 0.1 + "%";
   }
-  /* if (direccion === "bottom" && parseFloat(node.style.top) >= 0) {
-    node.style.top = (parseFloat(node.style.top) || 0) - 0.1 + "%"; */
-  if (direccion === "bottom" && parseFloat(node.style.bottom) <= 97) {
-    node.style.bottom = (parseFloat(node.style.bottom) || 0) + 0.1 + "%";
+  if (direccion === "bottom" && parseFloat(node.style.top) >= 0) {
+    node.style.top = (parseFloat(node.style.top) || 0) - 0.1 + "%";
   }
 }
